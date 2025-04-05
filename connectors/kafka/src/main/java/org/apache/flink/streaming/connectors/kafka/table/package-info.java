75d475a0b068f688af1db96c36307ac945d99d88
Ferenc Csaky <ferenc.csaky@pm.me>
2025-04-05 18:28:04 +0200

    Provide package-info.java for upstream package

---
+++ connectors/kafka/src/main/java/org/apache/flink/streaming/connectors/kafka/table/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The classes in this package are required to keep the original package of the relevant classes
+ * defined in the {@code flink-connector-kafka} project, because they are relying on package-private
+ * logic.
+ */
+package org.apache.flink.streaming.connectors.kafka.table;
