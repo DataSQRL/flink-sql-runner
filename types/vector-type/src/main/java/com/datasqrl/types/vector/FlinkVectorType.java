01de28208ec2597c2874765bcad50abc9dd77395
Marvin Froeder <marvin@datasqrl.com>
2025-04-17 21:29:18 -0300

    Extracted vector-type from sqrl project
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>

---
+++ types/vector-type/src/main/java/com/datasqrl/types/vector/FlinkVectorType.java
@@ -0,0 +1,19 @@
+package com.datasqrl.types.vector;
+
+import org.apache.flink.table.annotation.DataTypeHint;
+
+@DataTypeHint(
+    value = "RAW",
+    bridgedTo = FlinkVectorType.class,
+    rawSerializer = FlinkVectorTypeSerializer.class)
+public class FlinkVectorType {
+  public double[] value;
+
+  public FlinkVectorType(double[] value) {
+    this.value = value;
+  }
+
+  public double[] getValue() {
+    return value;
+  }
+}
