48014cf269b0a3a0277fdee6f8b7b60f97598028
Marvin Froeder <marvin@datasqrl.com>
2025-04-25 10:37:30 -0300

    Extract text-functions from sqrl project
    Signed-off-by: Marvin Froeder <marvin@datasqrl.com>

---
+++ functions/text-functions/src/test/java/com/datasqrl/function/text/FormatTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2024 DataSQRL (contact@datasqrl.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datasqrl.function.text;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FormatTest {
+
+  Format underTest = new Format();
+
+  @Test
+  public void testFormat() {
+    var format = "Hello, %s";
+    assertEquals("Hello, World", underTest.eval(format, "World"));
+    format = "Count: %s, %s, %s, %s";
+    assertEquals("Count: 1, 2, 3, 4", underTest.eval(format, "1", "2", "3", "4"));
+  }
+}
