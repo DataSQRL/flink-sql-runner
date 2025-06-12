6622c865d546f56e61f917a9feb95a5f934541ab
Ferenc Csaky <ferenc@datasqrl.com>
2025-06-12 09:10:38 +0200

    Add common functions from `sqrl` repo, unify function package naming

--- functions/openai-functions/src/test/java/com/datasqrl/flinkrunner/functions/openai/ExtractJsonTest.java
+++ functions/openai-functions/src/test/java/com/datasqrl/flinkrunner/functions/openai/ExtractJsonTest.java
@@ -15,14 +15,13 @@
  */
 package com.datasqrl.flinkrunner.functions.openai;
 
-import static com.datasqrl.flinkrunner.stdlib.openai.util.FunctionMetricTracker.*;
+import static com.datasqrl.flinkrunner.functions.openai.util.FunctionMetricTracker.CALL_COUNT;
+import static com.datasqrl.flinkrunner.functions.openai.util.FunctionMetricTracker.ERROR_COUNT;
 import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
-import com.datasqrl.flinkrunner.stdlib.openai.OpenAICompletions;
-import com.datasqrl.flinkrunner.stdlib.openai.extract_json;
 import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
