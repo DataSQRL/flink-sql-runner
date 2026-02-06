393fb21c7a8cec965c270cb03ed8eaf61dd4430e
Ferenc Csaky <ferenc@datasqrl.com>
2026-02-06 13:46:17 +0100

    Update license header year to 2026

--- flink-sql-runner/src/main/java/org/apache/flink/formats/avro/package-info.java
+++ flink-sql-runner/src/main/java/org/apache/flink/formats/avro/package-info.java
@@ -1,9 +1,11 @@
 /*
- * Copyright © 2025 DataSQRL (contact@datasqrl.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * We need to overwrite classes in this package downstream to apply some custom logic for the Kafka
  * deserialization failure handler to work properly.
--- scripts/install-git-hooks.sh
+++ scripts/install-git-hooks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright © 2025 DataSQRL (contact@datasqrl.com)
+# Copyright © 2026 DataSQRL (contact@datasqrl.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
